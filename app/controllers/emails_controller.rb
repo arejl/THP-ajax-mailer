bb7c404e21502bd6ea29ace1362f496648eae777
Ariane <pikabooariane@hotmail.fr>
2021-03-04 14:07:20 +0200

    Corrected bugs and added unread functionality

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -5,7 +5,7 @@ class EmailsController < ApplicationController
   end
 
   def create
-    @email = Email.new(object:Faker::TvShows::Friends.location, body:Faker::TvShows::Friends.quote, read:false)
+    @email = Email.new(object:Faker::TvShows::Friends.location, body:Faker::TvShows::Friends.quote, read: false)
     if @email.save 
       respond_to do |format|
         format.html {
@@ -17,12 +17,24 @@ class EmailsController < ApplicationController
     end
   end
 
+  def update
+    @email = Email.find(params[:id])
+    @email.update(read: false)
+    respond_to do |format|
+      format.html {
+       redirect_to root_path
+      }
+      format.js { }
+    end
+  end
+
   def show
     @email = Email.find(params[:id])
+    @email.update(read: true)
     respond_to do |format|
       format.html { }
       format.js { }
-    end
+    end    
   end
 
   def destroy
